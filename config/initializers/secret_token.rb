f88f030db528412e8bb79a01cc61a1a9f27dc2bf
Jorge Pardiñas <jorge.parbros@crowdint.com>
2012-06-06 17:15:05 -0500

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TallerMvp::Application.config.secret_token = 'fe8ad6becd96ed5248a5af46af697980e6086eacdcfe388a0e1284aa62da5b6b66817b6b056308f7149dfac6ecce81a054317db0ca4aae4469325456af95ffea'
